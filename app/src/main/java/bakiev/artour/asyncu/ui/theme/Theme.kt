e6549a3409ec176b0b74cfa2784c9582ed68492a
Artour Bakiev <abakiev@atlassian.com>
2022-11-23 23:21:18 +1100

    Dependencies updated

--- app/src/main/java/bakiev/artour/asyncu/ui/theme/Theme.kt
+++ app/src/main/java/bakiev/artour/asyncu/ui/theme/Theme.kt
@@ -13,7 +13,7 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -39,6 +39,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun AsyncUTheme(
+    activity: Activity,
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
@@ -55,8 +56,8 @@ fun AsyncUTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            activity.window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(activity.window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
