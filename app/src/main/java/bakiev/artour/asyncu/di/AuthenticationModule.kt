3c5cc49cee1c2a6d4644613da38b7a3b151ec412
Artour Bakiev <abakiev@atlassian.com>
2022-11-25 13:19:01 +1100

    `DefaultGoogleSignInService` has been retired

--- app/src/main/java/bakiev/artour/asyncu/di/AuthenticationModule.kt
+++ app/src/main/java/bakiev/artour/asyncu/di/AuthenticationModule.kt
@@ -1,8 +1,11 @@
 package bakiev.artour.asyncu.di
 
 import android.content.Context
-import bakiev.artour.asyncu.services.DefaultGoogleSignInService
-import bakiev.artour.asyncu.services.GoogleSignInService
+import android.content.Intent
+import bakiev.artour.asyncu.services.GoogleAuthenticationService
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,6 +17,24 @@ import dagger.hilt.components.SingletonComponent
 class AuthenticationModule {
 
     @Provides
-    fun provideGoogleSignInTest(@ApplicationContext context: Context): GoogleSignInService =
-        DefaultGoogleSignInService(context)
+    fun provideGoogleAuthenticationService(@ApplicationContext context: Context): GoogleAuthenticationService =
+        object : GoogleAuthenticationService {
+
+            private val googleSignInClient by lazy(mode = LazyThreadSafetyMode.NONE) {
+                val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestEmail()
+                    .build()
+                GoogleSignIn.getClient(context, signInOptions)
+            }
+
+            override val lastSignedInAccount: GoogleSignInAccount?
+                get() = GoogleSignIn.getLastSignedInAccount(context)
+
+            override val signInIntent: Intent
+                get() = googleSignInClient.signInIntent
+
+            override fun signOut() {
+                googleSignInClient.signOut()
+            }
+        }
 }
--- app/src/main/java/bakiev/artour/asyncu/ui/MainViewModel.kt
+++ app/src/main/java/bakiev/artour/asyncu/ui/MainViewModel.kt
@@ -1,29 +1,52 @@
 package bakiev.artour.asyncu.ui
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import bakiev.artour.asyncu.services.GoogleSignInService
+import bakiev.artour.asyncu.services.GoogleAuthenticationService
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val googleSignInService: GoogleSignInService) : ViewModel() {
+class MainViewModel @Inject constructor(private val googleAuthenticationService: GoogleAuthenticationService) :
+    ViewModel() {
 
-    var googleSignInAccount: GoogleSignInAccount? by mutableStateOf(googleSignInService.lastSignInAccount)
+    var googleSignInAccount: GoogleSignInAccount? by mutableStateOf(googleAuthenticationService.lastSignedInAccount)
+        private set
 
     @Composable
     fun rememberGoogleSignInLauncher() =
-        rememberLauncherForActivityResult(contract = googleSignInService.signInContract()) {
-            googleSignInAccount = googleSignInService.lastSignInAccount
+        rememberLauncherForActivityResult(contract = SignInContract(googleAuthenticationService.signInIntent)) {
+            googleSignInAccount = googleAuthenticationService.lastSignedInAccount
         }
 
     fun googleSingOut() {
-        googleSignInService.signOut()
-        googleSignInAccount = googleSignInService.lastSignInAccount
+        googleAuthenticationService.signOut()
+        googleSignInAccount = googleAuthenticationService.lastSignedInAccount
+    }
+
+    private class SignInContract(private val signInIntent: Intent) :
+        ActivityResultContract<Void?, GoogleSignInAccount?>() {
+        override fun createIntent(context: Context, input: Void?): Intent = signInIntent
+
+        override fun parseResult(resultCode: Int, intent: Intent?): GoogleSignInAccount? =
+            if (resultCode == Activity.RESULT_OK) {
+                // The Task returned from this call is always completed, no need to attach a listener.
+                val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(intent)
+                task.getResult(ApiException::class.java)
+            } else {
+                null
+            }
     }
 }
