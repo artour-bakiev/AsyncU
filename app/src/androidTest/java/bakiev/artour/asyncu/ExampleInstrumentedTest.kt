93bb7f273f63fedd92ac207bd0fe02100f51380c
Artour Bakiev <artour.bakiev@gmail.com>
2023-05-26 22:36:46 +1000

    `DefaultGoogleSignInService` has been retired

--- app/src/androidTest/java/bakiev/artour/asyncu/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/bakiev/artour/asyncu/ExampleInstrumentedTest.kt
@@ -1,22 +1,15 @@
 package bakiev.artour.asyncu
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
+import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Test
-import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
+    fun shouldUseApplicationContext() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("bakiev.artour.asyncu", appContext.packageName)
+
+
+        appContext.packageName shouldBeEqualTo "bakiev.artour.asyncu"
     }
 }
---
+++ app/src/main/java/bakiev/artour/asyncu/services/GoogleAuthenticationService.kt
@@ -0,0 +1,12 @@
+package bakiev.artour.asyncu.services
+
+import android.content.Intent
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+
+interface GoogleAuthenticationService {
+    val lastSignedInAccount: GoogleSignInAccount?
+
+    val signInIntent: Intent
+
+    fun signOut()
+}
