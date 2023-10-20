f6178bd02ffb90a18179f368504058ec77bc77a2
Artour Bakiev <Artour.Bakiev@gmail.com>
2023-10-21 00:03:33 +1100

    Dependency update - bumping to AGP 8.1.2 and Kotlin 1.9.10

--- app/src/main/java/bakiev/artour/asyncu/ui/Routes.kt
+++ app/src/main/java/bakiev/artour/asyncu/ui/Routes.kt
@@ -1,6 +1,6 @@
 package bakiev.artour.asyncu.ui
 
 sealed class Routes(val route: String) {
-    object Home : Routes("home")
-    object GoogleDrive : Routes("google-drive")
+    data object Home : Routes("home")
+    data object GoogleDrive : Routes("google-drive")
 }
